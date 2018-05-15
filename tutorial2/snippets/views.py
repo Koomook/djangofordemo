02394e8c8fe569aeed5ab20a2e3bb8edc3fee6ec
koomook <bongbonggg97@gmail.com>
2018-05-16 01:37:10 +0900

    Change to class on my own

--- tutorial2/snippets/views.py
+++ tutorial2/snippets/views.py
@@ -22,27 +22,25 @@ class SnippetList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk, format=None):
+class SnippetDetail(APIView):
     """
-    Retrieve, update or delete a code snippet.
+    Retrive, update or delete a snippet instance
     """
-    try:
-        snippet = Snippet.objects.get(pk=pk) # pk == primary key
-    except Snippet.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = SnippetSerializer(snippet)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
+    def get_object(self, pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DeosNotExist:
+            raise Http404
+    
+    def update(self, request, pk, format=None):
+        snippet = self.get_object(pk=pk)
         serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
+    
+    def delete(self, pk):
+        snippet = self.get_object(pk=pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
