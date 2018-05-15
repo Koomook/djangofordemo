c0017c8403bd1b42bb43ca58e92e02d60a0c2142
koomook <bongbonggg97@gmail.com>
2018-05-15 18:01:02 +0900

    Add hosts to delpoy on tester

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# add hosts
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'tester.pozalabs.com']
