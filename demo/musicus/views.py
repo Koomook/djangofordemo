ce0a5583ad08d0293f90f579fb4c17b710ed9d52
koomook <bongbonggg97@gmail.com>
2018-05-16 23:57:08 +0900

    Add src : seperate api and models

--- demo/musicus/views.py
+++ demo/musicus/views.py
@@ -5,11 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-import os
-import numpy as np
-mp3dir = 'mp3'
-mp3s = [os.path.join(mp3dir, fn) for fn in os.listdir(mp3dir)]
-print(mp3s)
+from .src.song import pickmp3
 
 class Hello(APIView):
     """
@@ -21,7 +17,5 @@ class Hello(APIView):
 class Song(APIView):
     """pick random song"""
     def get(self, request, format=None):
-        selected = np.random.choice(mp3s, 1)[0]
-        song = "<audio src='{}' controls controlsList='nodownload'></audio>".format(
-            selected)
-        return Response({'song':'song'}, status=status.HTTP_200_OK)
+        mp3 = pickmp3()
+        return Response({'song':mp3}, status=status.HTTP_200_OK)
