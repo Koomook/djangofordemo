24a10991809e02761480aa107175e47014499ccb
koomook <bongbonggg97@gmail.com>
2018-05-16 19:33:16 +0900

    Initial Commit just say hello pozalbs

---
+++ demo/musicus/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+from musicus import views
+
+urlpatterns = [
+    url(r'^test/$', views.Hello.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- demo/musicus/views.py
+++ demo/musicus/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# from snippets.models import Snippet
+# from snippets.serializers import SnippetSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class Hello(APIView):
+    """
+    test api
+    """
+    def get(self, request, format=None):
+        return Response(data={'hello':'pozalabs'}, status=status.HTTP_200_OK)
