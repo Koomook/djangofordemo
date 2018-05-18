cca134168779dcb9aa0e61c6ba75a5c82678c11c
koomook <bongbonggg97@gmail.com>
2018-05-18 15:58:08 +0900

    Change static url to dynamic : not sure it's good wording

--- demo/musicus/urls.py
+++ demo/musicus/urls.py
@@ -5,7 +5,7 @@ from musicus import views
 urlpatterns = [
     url(r'^test/$', views.Hello.as_view()),
     url(r'^song/$', views.Song.as_view()),
-    url(r'^lyrics/kor/02/(?P<slug>[\w,\s-]+)/$', views.Lyrics.as_view()),
+    url(r'^lyrics/(?P<language>kor|eng)/(?P<ver>0\d)/(?P<slug>[\w,\s-]+)/$', views.Lyrics.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
