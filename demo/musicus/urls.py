3ef55ebca2d2b23d28d4325cf298c89a8b9331b4
koomook <bongbonggg97@gmail.com>
2018-05-18 21:31:39 +0900

    Add song pick urls via pk

--- demo/musicus/urls.py
+++ demo/musicus/urls.py
@@ -4,7 +4,8 @@ from musicus import views
 
 urlpatterns = [
     url(r'^test/$', views.Hello.as_view()),
-    url(r'^song/$', views.Song.as_view()),
+    url(r'^song/$', views.SongList.as_view()),
+    url(r'^song/(?P<pk>[0-9]+)/$', views.SongDetail.as_view()),
     url(r'^lyrics/(?P<language>kor|eng)/(?P<ver>0\d)/(?P<slug>[\w,\s-]+)/$', views.Lyrics.as_view()),
 ]
 
