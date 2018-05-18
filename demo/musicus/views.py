0f2727ea52332aa7e637cf54cfb9da0a2fc706bb
koomook <bongbonggg97@gmail.com>
2018-05-18 15:58:27 +0900

    Change code structure

--- demo/musicus/views.py
+++ demo/musicus/views.py
@@ -1,20 +1,29 @@
-# from snippets.models import Snippet
-# from snippets.serializers import SnippetSerializer
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+import json
 
 from .src.song import pickmp3
-
-from .src.lyrics_core import load_model, generate
+from .src.lyrics import Models
+from .src.lyrics_gen import Gen
 batch_size = 3
-K2L_kor_infer, S2S_kor_infer = load_model(batch_size=batch_size)
+model = Models(batch_size=batch_size)
+gen = Gen()
+
+with open('src/meta.json') as fp:
+    meta = json.load(fp)
+K2L_kor_infer = model.load_K2L_kor(
+    meta['K2L_kor_project'], meta['K2L_kor_dir_path'], 0.8)
+S2S_kor_infer = model.load_S2S_kor(
+    meta['S2S_kor_project'], meta['S2S_kor_dir_path'], 0.8)
+
 
 class Hello(APIView):
     """
     test api
     """
+
     def get(self, request, format=None):
         print(request.data)
         return Response(data={'hello': 'pozalabs'}, status=status.HTTP_200_OK)
@@ -22,19 +31,23 @@ class Hello(APIView):
 
 class Song(APIView):
     """pick random song"""
+
     def get(self, request, format=None):
         mp3 = pickmp3()
-        return Response({'song': mp3}, status=status.HTTP_200_OK, headers={'Access-Control-Allow-Origin': '*'})
+        return Response({'song': mp3}, status=status.HTTP_200_OK)
+
 
 class Lyrics(APIView):
     """generate lyrics"""
-    def get(self, request, slug, format=None):
-        print(slug)
+
+    def get(self, request, language, ver, slug, format=None):
+        print(language, type(language))
+        print(ver, type(ver))
+        print(slug, type(slug))
         keyword_input = slug
-        spaced_lyrics = generate(keyword_input, S2S_kor_infer, K2L_kor_infer,
-                                        batch_size=batch_size, appending_size=2)
-
-        return Response({'lyrics':spaced_lyrics})
-        
-        
-
+        if language == 'kor' and ver == '02':
+            spaced_lyrics = gen.generate_kor02(
+                keyword_input, S2S_kor_infer, K2L_kor_infer, batch_size=batch_size, appending_size=2)
+            return Response({'lyrics': spaced_lyrics})
+        else:
+            return Response(Http404)
