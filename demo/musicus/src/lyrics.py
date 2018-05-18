cc5e2660b1bbcb75e8fc0590b1c4ae9f378baa19
koomook <bongbonggg97@gmail.com>
2018-05-18 16:40:55 +0900

    Fix module name

--- demo/musicus/src/lyrics.py
+++ demo/musicus/src/lyrics.py
@@ -22,6 +22,7 @@ class Models():
     
     def kor_model(self,
                   dir_path,
+                  model_class,
                   n_input=10, 
                   n_target=10, 
                   n_blocks=6, 
@@ -33,48 +34,51 @@ class Models():
             2. model_hyperparmas.json 등으로 parameters 관리
         """
         table = np.load(os.path.join(dir_path, 'table.npy'))
-        model = LyricistMultiConditions_Keyword(batch_size=None,
-                                                n_input=n_input,
-                                                n_target=n_target,
-                                                n_blocks=n_blocks,
-                                                n_heads=n_heads,
-                                                embedding_size=embedding_size,
-                                                projection_size=table.shape[0],
-                                                n_condition=4,
-                                                wv_table=table)
+        model = model_class(batch_size=None,
+                            n_input=n_input,
+                            n_target=n_target,
+                            n_blocks=n_blocks,
+                            n_heads=n_heads,
+                            embedding_size=embedding_size,
+                            projection_size=table.shape[0],
+                            n_condition=4,
+                            wv_table=table)
         model.build_graph()
         return model
 
-    def load_kor_infer(self, inferclass, project_name, dir_path, keep_prob):
+    def load_kor_infer(self, model_class, infer_class, project_name, dir_path, keep_prob):
         graph = tf.Graph()
         print(graph.__hash__)
         with graph.as_default():
-            model = self.kor_model(dir_path)
-            infer = inferclass(model=model,
-                               keep_prob=keep_prob,
-                               preprocessor=None,
-                               dir_path=dir_path,
-                               n_input=model.n_input,
-                               n_target=model.n_target,
-                               batch_size=self.batch_size,
-                               project_name=project_name,
-                               use_local_wv=True,
-                               is_eng=False)
+            model = self.kor_model(dir_path, model_class)
+            infer = infer_class(model=model,
+                                keep_prob=keep_prob,
+                                preprocessor=None,
+                                dir_path=dir_path,
+                                n_input=model.n_input,
+                                n_target=model.n_target,
+                                batch_size=self.batch_size,
+                                project_name=project_name,
+                                use_local_wv=True,
+                                is_eng=False)
             return infer
 
     def load_K2L_kor(self, project_name, dir_path, keep_prob=0.8):
         """@wrapper
         to make readible code
         """
-        inferclass = InferK2L_conditions
-        return self.load_kor_infer(inferclass, project_name, dir_path, keep_prob)
+        model_class = LyricistMultiConditions_Keyword
+        infer_class = InferK2L_conditions
+        
+        return self.load_kor_infer(model_class, infer_class, project_name, dir_path, keep_prob)
 
     def load_S2S_kor(self, project_name, dir_path, keep_prob=0.8):
         """@wrapper
         to make readible code
         """
-        inferclass = InferS2S_conditions
-        return self.load_kor_infer(inferclass, project_name, dir_path, keep_prob)
+        model_class = LyricistMultiConditions_Tag
+        infer_class = InferS2S_conditions
+        return self.load_kor_infer(model_class, infer_class, project_name, dir_path, keep_prob)
     
     def eng_infer(self, model, dir_path, project_name, keep_prob):
         infer = SingleInference(model=model,
